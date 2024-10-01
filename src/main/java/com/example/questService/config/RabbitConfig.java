e790de1c30988ad5e36b7b29e2b647cccda913c3
smetanka <spiralcka@gmail.com>
2024-10-01 11:19:56 +0300

    Fix rabbitMq

--- src/main/java/com/example/questService/config/RabbitConfig.java
+++ src/main/java/com/example/questService/config/RabbitConfig.java
@@ -1,14 +1,41 @@
 package com.example.questService.config;
 
-import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitConfig {
 
+    public static final String EXCHANGE_NAME = "questExchange";
+
     @Bean
-    public Queue questRequestQueue() {
-        return new Queue("questRequestQueue");
+    public DirectExchange questExchange() {
+        return new DirectExchange(EXCHANGE_NAME);
     }
+
+    @Bean
+    public Jackson2JsonMessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
+                                         Jackson2JsonMessageConverter messageConverter) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter);
+        return rabbitTemplate;
+    }
+
+
+//    @Bean
+//    public AmqpTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+//        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+//        rabbitTemplate.setMessageConverter(jsonMessageConverter());
+//        return rabbitTemplate;
+//    }
+
 }
--- src/main/java/com/example/questService/controller/QuestController.java
+++ src/main/java/com/example/questService/controller/QuestController.java
@@ -1,18 +1,48 @@
 package com.example.questService.controller;
 
 import com.example.questModel.Quest;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/quests")
 public class QuestController {
 
-    @GetMapping("random")
-    public Quest getRandomQuest() {
-        System.out.println("Get random quest");
-        return null;
+    private final AmqpTemplate rabbitTemplate;
+
+    @Autowired
+    public QuestController(AmqpTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
     }
 
+
+    @GetMapping("daily")
+    public List<Quest> getDailyQuests() {
+        System.out.println("Requesting daily quests");
+        // Отправляем запрос на очередь и ожидаем ответ
+        List<Quest> dailyQuests = (List<Quest>) rabbitTemplate.convertSendAndReceive("questExchange", "questRequestQueue", "");
+        return dailyQuests;
+    }
+
+    @GetMapping("{id}")
+    public Quest getQuestById(@PathVariable Long id) {
+        System.out.println("Requesting quest by id: " + id);
+        // Отправляем запрос с идентификатором и ожидаем ответ
+        Quest quest = (Quest) rabbitTemplate.convertSendAndReceive("questExchange", "questByIdQueue", id);
+        return quest;
+    }
+
+    @GetMapping("random")
+    public Quest getRandomQuest() {
+        System.out.println("Requesting random quest");
+        // Отправляем запрос на случайное задание и ожидаем ответ
+        Quest randomQuest = (Quest) rabbitTemplate.convertSendAndReceive("questExchange", "questRandomQueue", "");
+        return randomQuest;
+    }
 }
