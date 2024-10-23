cf5856eb844651d82ba4ed416a7b9da49e824754
smetanka <spiralcka@gmail.com>
2024-10-23 15:13:44 +0300

    Add Redis and Swagger, add new profiles for dev

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,8 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-amqp")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-redis")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.amqp:spring-rabbit-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
--- src/main/java/com/example/questService/controller/QuestController.java
+++ src/main/java/com/example/questService/controller/QuestController.java
@@ -1,8 +1,13 @@
 package com.example.questService.controller;
 
 import com.example.questModel.Quest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Tag(name = "main-methods")
 @RestController
 @RequestMapping("/quests")
 public class QuestController {
@@ -21,14 +27,42 @@ public class QuestController {
         this.rabbitTemplate = rabbitTemplate;
     }
 
-
-    @GetMapping("daily")
+    @Operation(
+            summary = "Возвращает три случайных квеста из БД",
+            description = "Этот метод возвращает три случайных квеста, которые были выбраны из базы данных. " +
+                    "Эти квесты обновляются ежедневно и могут быть использованы для выполнения в течение дня." +
+                    "Ответ кэшируется в Redis на 1 минуту "
+    )
+    @GetMapping("/daily")
+    @Cacheable(value = "dailyQuests")
     public List<Quest> getDailyQuests() {
-        System.out.println("Requesting daily quests");
-        List<Quest> dailyQuests = (List<Quest>) rabbitTemplate.convertSendAndReceive("questExchange", "dailyQuests", "");
-        return dailyQuests;
+        System.out.println("Получение ежедневных квестов из RabbitMQ");
+        return (List<Quest>) rabbitTemplate.convertSendAndReceive("questExchange", "dailyQuests", "");
     }
 
+
+    @Operation(
+            summary = "Попытка сделать кофе",
+            description = "Эндпоинт возвращает HTTP статус 418 (I'm a teapot) с сообщением о том, что сервер — это чайник и не может приготовить кофе."
+    )
+    @GetMapping("/makeCoffee")
+    public ResponseEntity<String> makeCoffee() {
+        return ResponseEntity.status(HttpStatus.I_AM_A_TEAPOT).body("I'm a teapot, I cannot make coffee");
+    }
+
+
+//    @GetMapping("daily")
+//    public List<Quest> getDailyQuests() {
+//        System.out.println("Requesting daily quests");
+//        List<Quest> dailyQuests = (List<Quest>) rabbitTemplate.convertSendAndReceive("questExchange", "dailyQuests", "");
+//        return dailyQuests;
+//    }
+
+
+    @Operation(
+            summary = "Получить квест по ID",
+            description = "Возвращает квест из базы данных по указанному идентификатору. ID передается в запросе как путь параметра."
+    )
     @GetMapping("{id}")
     public Quest getQuestById(@PathVariable Long id) {
         System.out.println("Requesting quest by id: " + id);
@@ -36,6 +70,10 @@ public class QuestController {
         return quest;
     }
 
+    @Operation(
+            summary = "Получить случайный квест",
+            description = "Возвращает случайный квест из базы данных."
+    )
     @GetMapping("random")
     public Quest getRandomQuest() {
         System.out.println("Requesting random quest");
